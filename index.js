d36b80c5df2ddc3433e0288de83c6c4de9bb31f1
delvedor <tommydelved@gmail.com>
2018-04-01 19:50:24 +0200

    Plugin code
    à Plugin code

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+// External utilities
+const forwarded = require('forwarded')
+const proxyaddr = require('proxy-addr')
+// Internals Utilities
+const httpErrors = require('./lib/httpErrors')
+const assert = require('./lib/assert')
+const vary = require('./lib/vary')
+
+function fastifySensible (fastify, opts, next) {
+  fastify.decorate('httpErrors', httpErrors)
+  fastify.decorate('assert', assert)
+  fastify.decorate('to', to)
+
+  fastify.decorateRequest('forwarded', function () {
+    return forwarded(this.raw)
+  })
+
+  fastify.decorateRequest('proxyaddr', function (trust) {
+    return proxyaddr(this.raw, trust)
+  })
+
+  fastify.decorateReply('vary', vary)
+
+  // TODO: benchmark if this closure causes some performance drop
+  Object.keys(httpErrors).forEach(httpError => {
+    fastify.decorateReply(httpError, function (message) {
+      this.send(httpErrors[httpError](message))
+    })
+  })
+
+  function to (promise) {
+    return promise.then(data => [null, data], err => [err, undefined])
+  }
+
+  next()
+}
+
+module.exports = fp(fastifySensible, {
+  name: 'fastify-sensible',
+  fastify: '1.x'
+})
