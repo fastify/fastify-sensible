90be0ebaa071bada60e4a8d0da59821ed857df23
Vlad <vlad.betcun@gmail.com>
2021-07-18 12:03:14 +0300

    test: increase code coverage to 100%. Closes #56

--- test/httpErrorsReply.test.js
+++ test/httpErrorsReply.test.js
@@ -7,14 +7,8 @@ const Sensible = require('../index')
 
 // from Node.js v10 and above the 418 message has been changed
 const node10 = Number(process.versions.node.split('.')[0]) >= 10
-
-// fix unsupported status codes
-const unsupported = [425]
-for (const code in statusCodes) {
-  if (unsupported.includes(Number(code))) {
-    delete statusCodes[code]
-  }
-}
+// from Node.js v14 and above the 425 message has been changed
+const node14 = Number(process.versions.node.split('.')[0]) >= 14
 
 test('Should generate the correct http error', t => {
   Object.keys(statusCodes).forEach(code => {
@@ -42,7 +36,53 @@ test('Should generate the correct http error', t => {
             message: 'I\'m a teapot',
             statusCode: 418
           })
+          // should be deleted after release of https://github.com/jshttp/http-errors/pull/73
+        } else if (code === '425') {
+          t.same(JSON.parse(res.payload), {
+            error: node14 ? 'Too Early' : 'Unordered Collection',
+            message: 'Unordered Collection',
+            statusCode: 425
+          })
         } else {
+          t.same(JSON.parse(res.payload), {
+            error: statusCodes[code],
+            message: statusCodes[code],
+            statusCode: Number(code)
+          })
+        }
+      })
+    })
+  })
+  t.end()
+})
+
+test('Should generate the correct http error using getter', t => {
+  Object.keys(statusCodes).forEach(code => {
+    if (Number(code) < 400) return
+    t.test(code, t => {
+      t.plan(3)
+      const fastify = Fastify()
+      fastify.register(Sensible)
+
+      fastify.get('/', (req, reply) => {
+        reply.getHttpError(code)
+      })
+
+      fastify.inject({
+        method: 'GET',
+        url: '/'
+      }, (err, res) => {
+        t.error(err)
+        // should be deleted after release of https://github.com/jshttp/http-errors/pull/73
+        if ((node10 && code === '418') || (node14 && code === '425')) {
+          t.equal(res.statusCode, 500)
+          t.same(JSON.parse(res.payload), {
+            error: 'Internal Server Error',
+            message: 'Something went wrong',
+            statusCode: 500
+          })
+        } else {
+          t.equal(res.statusCode, Number(code))
           t.same(JSON.parse(res.payload), {
             error: statusCodes[code],
             message: statusCodes[code],
@@ -88,6 +128,9 @@ test('Should generate the correct http error (with custom message)', t => {
 function normalize (code, msg) {
   if (code === '414') return 'uriTooLong'
   if (code === '418') return 'imateapot'
+  // rename of supported tooEarly to the unsupported unorderedCollection
+  // should be deleted after release of https://github.com/jshttp/http-errors/pull/73
+  if (code === '425') return 'unorderedCollection'
   if (code === '505') return 'httpVersionNotSupported'
   msg = msg.split(' ').join('').replace(/'/g, '')
   msg = msg[0].toLowerCase() + msg.slice(1)
