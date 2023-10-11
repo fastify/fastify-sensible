9061d6b9fb7c2a1fcc1ebfcc2f856c353bdda1b1
Arthur Fiorette <me@arthur.place>
2023-10-11 13:43:46 -0300

    also re-export HttpErrorReplys

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,25 +4,23 @@ import * as Errors from '../lib/httpError'
 
 type FastifySensible = FastifyPluginCallback<fastifySensible.SensibleOptions>
 
-type singleValueTypes = 'must-revalidate' |
-  'no-cache' |
-  'no-store' |
-  'no-transform' |
-  'public' |
-  'private' |
-  'proxy-revalidate' |
-  'immutable'
+type singleValueTypes =
+  | 'must-revalidate'
+  | 'no-cache'
+  | 'no-store'
+  | 'no-transform'
+  | 'public'
+  | 'private'
+  | 'proxy-revalidate'
+  | 'immutable'
 
-type multiValueTypes = 'max-age' |
-  's-maxage' |
-  'stale-while-revalidate' |
-  'stale-if-error'
+type multiValueTypes =
+  | 'max-age'
+  | 's-maxage'
+  | 'stale-while-revalidate'
+  | 'stale-if-error'
 
 type staleTypes = 'while-revalidate' | 'if-error'
-
-type HttpErrorReplys = {
-  getHttpError: (code: HttpErrorCodes, message?: string) => FastifyReply;
-} & Record<HttpErrorNames, (msg?: string) => FastifyReply>;
 
 declare module 'fastify' {
   namespace SensibleTypes {
@@ -46,7 +44,7 @@ declare module 'fastify' {
     httpErrors: HttpErrors;
   }
 
-  interface FastifyReply extends HttpErrorReplys {
+  interface FastifyReply extends fastifySensible.HttpErrorReplys {
     vary: {
       (field: string | string[]): void;
       append: (header: string, field: string | string[]) => string;
@@ -94,6 +92,11 @@ declare namespace fastifySensible {
   export type HttpErrors = Errors.HttpErrors;
   export type HttpErrorCodes = Errors.HttpErrorCodes;
   export type HttpErrorNames = Errors.HttpErrorNames;
+
+
+  export type HttpErrorReplys = {
+    getHttpError: (code: HttpErrorCodes, message?: string) => FastifyReply;
+  } & Record<HttpErrorNames, (msg?: string) => FastifyReply>
 
   export const fastifySensible: FastifySensible
   export { fastifySensible as default }
